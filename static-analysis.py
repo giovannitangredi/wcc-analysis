0f41b5b17dd6193d51475745ad707b14d9dae0e3
giovannitangredi <giovanni.tangredi1@libero.it>
2022-05-05 13:49:01 +0200

    better folder structure, modify time-analysis.py and static-analysis.py

--- static-analysis.py
+++ static-analysis.py
@@ -10,18 +10,21 @@ from matplotlib.pyplot import figure
 def print_plot(x,vec,img_path,title): 
    
     X_axis = np.arange(len(x))
-    avg=[]
-    min=[]
-    max=[]
+    avg_cyc= []
+    max_cyc= []
+    avg_cogn= []
+    max_cogn= []
     for rows in vec:
-        avg.append(rows[0])
-        min.append(rows[1])
-        max.append(rows[2])
+        avg_cyc.append(rows[0])
+        max_cyc.append(rows[2])
+        avg_cogn.append(rows[1])
+        max_cogn.append(rows[3])
     plt.rcParams.update({'font.size': 12})
     figure(figsize=(20,16), dpi=80)
-    plt.bar(X_axis - 0.2, avg, 0.2, label = 'AVG')
-    plt.bar(X_axis, min, 0.2, label = 'MIN')
-    plt.bar(X_axis + 0.2, max, 0.2, label = 'MAX')
+    plt.bar(X_axis - 0.3, avg_cyc, 0.2, label = 'AVG Cyclomatic', color='#009302')
+    plt.bar(X_axis - 0.1, max_cyc, 0.2, label = 'MAX Cyclomatic', color='#000FFF')
+    plt.bar(X_axis + 0.1, avg_cogn, 0.2, label = 'AVG Cognitive', color='#00FF03')
+    plt.bar(X_axis + 0.3, max_cogn, 0.2, label = 'MAX Cognitive', color='#747BF0')
     plt.xticks(X_axis, x)
     plt.xlabel("projects")
     plt.ylabel("Complexity")
@@ -33,9 +36,11 @@ def print_plot(x,vec,img_path,title):
 
 def static_analysis(path_to_csvs,image_name):
     files = []
-    avg= []
-    min= []
-    max= []
+    avg_cyc= []
+    max_cyc= []
+    avg_cogn= []
+    max_cogn= []
+    n_complex= []
     for file in os.listdir(path_to_csvs):
         if file.endswith(".csv"):
             files.append(file)
@@ -47,29 +52,37 @@ def static_analysis(path_to_csvs,image_name):
             header = next(csvreader)
             for row in csvreader:
                 if row[0] == 'AVG':
-                    avg.append([float(row[1]),float(row[2]),float(row[3]),float(row[4])])
-                if row[0] == 'MIN':
-                    min.append([float(row[1]),float(row[2]),float(row[3]),float(row[4])])
+                    if file.split("_")[1] == "cyc.csv":
+                        avg_cyc.append([float(row[1]),float(row[2]),float(row[3]),float(row[4])])
+                    else:
+                        avg_cogn.append([float(row[1]),float(row[2]),float(row[3]),float(row[4])])
                 if row[0] == 'MAX':
-                    max.append([float(row[1]),float(row[2]),float(row[3]),float(row[4])])
+                     if file.split("_")[1] == "cyc.csv":
+                        max_cyc.append([float(row[1]),float(row[2]),float(row[3]),float(row[4])])
+                     else:
+                        max_cogn.append([float(row[1]),float(row[2]),float(row[3]),float(row[4])])
+                if row[0] == "TOTAL COMPLEX FILES":
+                    n_complex.append(float(row[1]))
     x=[]
     for f in files:
         no_ext_f = f[:len(f) - 4]
-        x.append(no_ext_f)
+        split_m = no_ext_f.split("_")
+        if split_m[1]=="cogn":
+            x.append(split_m[0])
 
     sifis=[]
     sifis_q=[]
     crap=[]
     skunk=[]
-    for i in range(len(avg)):
-        sifis.append( [avg[i][0], min[i][0], max[i][0]])
-        sifis_q.append( [avg[i][1], min[i][1], max[i][1]])
-        crap.append( [avg[i][2], min[i][2], max[i][2]])
-        skunk.append( [avg[i][3], min[i][3], max[i][3]])
-    print_plot(x,sifis,'./img/'+image_name+'_sifis.png',"Sifis")
-    print_plot(x,sifis_q,'./img/'+image_name+'_sifis_quantized.png',"Sifis Quantized")
-    print_plot(x,crap,'./img/'+image_name+'_crap.png',"CRAP")
-    print_plot(x,skunk,'./img/'+image_name+'_skunk.png',"SkunkScore")
+    for i in range(len(avg_cyc)):
+        sifis.append( [avg_cyc[i][0], avg_cogn[i][0], max_cyc[i][0], max_cogn[i][0]])
+        sifis_q.append( [avg_cyc[i][1], avg_cogn[i][1], max_cyc[i][1], max_cogn[i][1]])
+        crap.append( [avg_cyc[i][2], avg_cogn[i][2], max_cyc[i][2], max_cogn[i][2]])
+        skunk.append( [avg_cyc[i][3], avg_cogn[i][3], max_cyc[i][3], max_cogn[i][3]])
+    print_plot(x,sifis,'./img/Static/'+image_name+'_sifis.png',"Sifis")
+    print_plot(x,sifis_q,'./img/Static/'+image_name+'_sifis_quantized.png',"Sifis Quantized")
+    print_plot(x,crap,'./img/Static/'+image_name+'_crap.png',"CRAP")
+    print_plot(x,skunk,'./img/Static/'+image_name+'_skunk.png',"SkunkScore")
     return
 
 static_analysis("./StaticAnalysis","static_analysis")
--- time-analysis.py
+++ time-analysis.py
@@ -2,6 +2,7 @@ import os
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
 
 def print_plot(versions,vec,img_path):
     sifis=[]
@@ -16,18 +17,34 @@ def print_plot(versions,vec,img_path):
     plt.rcParams.update({'font.size': 12})
     figure, axis = plt.subplots(2, 2)
     figure.set_size_inches(24, 20)
-    axis[0, 0].plot(versions, sifis)
+    axis[0, 0].plot(versions, sifis, "r-")
+    axis[0, 0].bar(versions, sifis, width=0.5)
     axis[0, 0].set_title("Sifis Plain")
     axis[0, 0].set_xlabel('versions')
-    axis[0, 1].plot(versions, sifis_q)
+    axis[0, 1].plot(versions, sifis_q, "r-")
+    axis[0, 1].bar(versions, sifis_q, width=0.5)
     axis[0, 1].set_title("Sifis Quantized")
     axis[0, 1].set_xlabel('versions')
-    axis[1, 0].plot(versions, crap)
+    axis[1, 0].plot(versions, crap, "r-")
+    axis[1, 0].bar(versions, crap, width=0.5)
     axis[1, 0].set_title("CRAP")
     axis[1, 0].set_xlabel('versions')
-    axis[1, 1].plot(versions, skunk)
+    axis[1, 1].plot(versions, skunk, "r-")
+    axis[1, 1].bar(versions, skunk, width=0.5)
     axis[1, 1].set_title("SkunkScore")
     axis[1, 1].set_xlabel('versions')
+    plt.savefig(img_path)
+    plt.cla()
+    return
+
+def plot_complex(versions,vec,img_path):
+    plt.rcParams.update({'font.size': 12})
+    figure(figsize=(20,16), dpi=80)
+    plt.plot(versions, vec, "r-")
+    plt.bar(versions, vec, 0.5)
+    plt.xlabel("versions")
+    plt.ylabel("Number of complex files")
+    plt.title("Number of complex files")
     plt.savefig(img_path)
     plt.cla()
     return
@@ -35,8 +52,8 @@ def print_plot(versions,vec,img_path):
 def time_analysis(path_to_csvs, image_name) :
     files = []
     avg= []
-    min= []
     max= []
+    n_complex =[]
     for file in os.listdir(path_to_csvs):
         if file.endswith(".csv"):
             files.append(file)
@@ -49,18 +66,18 @@ def time_analysis(path_to_csvs, image_name) :
             for row in csvreader:
                 if row[0] == 'AVG':
                     avg.append([float(row[1]),float(row[2]),float(row[3]),float(row[4])])
-                if row[0] == 'MIN':
-                    min.append([float(row[1]),float(row[2]),float(row[3]),float(row[4])])
                 if row[0] == 'MAX':
                     max.append([float(row[1]),float(row[2]),float(row[3]),float(row[4])])
+                if row[0] == "TOTAL COMPLEX FILES":
+                    n_complex.append(float(row[1]))
     versions=[]
     for f in files:
         no_ext_f = f[:len(f) - 4]
         versions.append("-".join(no_ext_f.split("-")[1:]))
 
-    print_plot(versions,avg,'./img/'+image_name+'_avg.png')
-    print_plot(versions,min,'./img/'+image_name+'_min.png')
-    print_plot(versions,max,'./img/'+image_name+'_max.png')
+    print_plot(versions,avg,'./img/Time/'+image_name+'_avg.png')
+    print_plot(versions,max,'./img/Time/'+image_name+'_max.png')
+    plot_complex(versions,n_complex,'./img/Time/'+image_name+'_complex_files.png')
     return
 
 time_analysis("./TimeAnalysis/rust-analyzer","rust-analyzer")
